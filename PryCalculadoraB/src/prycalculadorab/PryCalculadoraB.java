2818d48031a5c8cb7d9393e67d313ed9df5b36ef
Natilu <38574035+natilu01@users.noreply.github.com>
2018-11-27 05:48:58 -0500

    Add files via upload

---
+++ PryCalculadoraB/src/prycalculadorab/PryCalculadoraB.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package prycalculadorab;
+
+/**
+ *
+ * @author USUARIO
+ */
+public class PryCalculadoraB {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       WinCalculadora ventana;
+       ventana=new WinCalculadora ();
+       ventana.setVisible(true);
+    }
+    
+}
